d5d6e5b1eec685099c6209da9589aaa47ce853fb
elishatherene <86765087+elishatherene@users.noreply.github.com>
2022-10-02 21:44:30 -0700

    Update factListJuno.js

--- factListJuno.js
+++ factListJuno.js
@@ -1,12 +1,12 @@
 var factList = [
-  "Juno was launched on August 5th, 2011 on an Atlas V-551 rocket";
-  "The telescope arrived at Jupiter on July 4th, 2016 after traveling 1,740 milion miles.";
-  "Juno's mission was to primarily understand the origin and evolution process of Jupiter by doing extensive searches for a solid planetary core, observing the auroras, measuring the water and ammonia in the depths of its' atmosphere, and mapping the magnetic field of the planet.";
-  "Juno orbited Jupiter on a 53-day polar orbit. It completeted its primary mission after 35 orbits.";
-  "The Juno mission was extended until September 2025 (or the end of its life) to enable the spacecraft to continue research the Jovian sustem and conduct flybys of Europa and Io (the two largest moons of Jupiter)";
-  "Juno is a spinning, solar-powered spacecraft that uses a highly elliptical polar orbit (two rotations per minute) that is stable and easy to control. It is the first the first solar-powered spacecraft desgined by NASA to operate so far from the Sun";
-  "The spacecraft is 66 ft (20 meters) in diameter and 15 ft (4.5 meters) in height";
-  "Juno uses many scienfitic instruments including a gravity science magnetometer (used to study the structure of Jupiter through the graity field), a JunoCam (to take close up images in color), a microwave radiometer (to observe the atmosphere/oxygen), a UVS/JIRAM ultraviolet/infrared camera (to take images of the aurora and atmosphere), and JEDI/JADE/Waves to observe the electrical fields and particles and connect the auroras to the magnetic field).";
+  "Juno was launched on August 5th, 2011 on an Atlas V-551 rocket",
+  "The telescope arrived at Jupiter on July 4th, 2016 after traveling 1,740 milion miles.",
+  "Juno's mission was to primarily understand the origin and evolution process of Jupiter by doing extensive searches for a solid planetary core, observing the auroras, measuring the water and ammonia in the depths of its' atmosphere, and mapping the magnetic field of the planet.",
+  "Juno orbited Jupiter on a 53-day polar orbit. It completeted its primary mission after 35 orbits.",
+  "The Juno mission was extended until September 2025 (or the end of its life) to enable the spacecraft to continue research the Jovian sustem and conduct flybys of Europa and Io (the two largest moons of Jupiter)",
+  "Juno is a spinning, solar-powered spacecraft that uses a highly elliptical polar orbit (two rotations per minute) that is stable and easy to control. It is the first the first solar-powered spacecraft desgined by NASA to operate so far from the Sun",
+  "The spacecraft is 66 ft (20 meters) in diameter and 15 ft (4.5 meters) in height",
+  "Juno uses many scienfitic instruments including a gravity science magnetometer (used to study the structure of Jupiter through the graity field), a JunoCam (to take close up images in color), a microwave radiometer (to observe the atmosphere/oxygen), a UVS/JIRAM ultraviolet/infrared camera (to take images of the aurora and atmosphere), and JEDI/JADE/Waves to observe the electrical fields and particles and connect the auroras to the magnetic field).",
 ];
 
  /* 
