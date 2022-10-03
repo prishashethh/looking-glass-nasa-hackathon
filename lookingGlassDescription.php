958c8e1b4b788adcf7ac7425c0d5e587d833bc22
elishatherene <86765087+elishatherene@users.noreply.github.com>
2022-10-02 23:27:57 -0700

    Update lookingGlassDescription.php

--- lookingGlassDescription.php
+++ lookingGlassDescription.php
@@ -45,68 +45,95 @@
       <body>
       <div class="banner-area" aria-label ="gradient background named moonlit asteroid">
         <div >
-          <h2 class="wrapper">explore different types of NASA telescopes and observatories that have revolutionized astronomical discoveries countless of times!</h2>
-          <p>click on an image below to learn more!</p>
+          <h1 class="wrapper">explore different types of NASA telescopes and observatories that have revolutionized astronomical discoveries countless of times!</h2>
+          <h2>Here are some fun facts about the James Webb Space Telescope</h2>
+          <p>the James Webb Space Telescope weighs nearly 13,700 pounds</p>
+          <p>the primary mirror is over 21 feet in diameter</p>
+          <p>the sunshield is approximately 69.5 ft by 46.5 ft (almost the size of a tennis court!)</p>
+          <p>the telescope is designed to help astronomers comprehend the formation of galaxies throughout billions of years using infrared light</p>
+          <p>Webb can see through dust clouds (where planetary systems and stars are formed) and studies atmospheres of other planets.</p>
+          <p>it orbits the Sun at a point located a milion miles (1.5 million km) from Earth known as the second Lagrange point (reffered to as L2)</p>
+          <p>the telescope was launched on december 25, 2021</p>
+          <p>the James Webb telescope project began construction in 2004.</p>
+          <p>the Webb mission is intended to last for 5.5 to 10 years.</p>
+          <p>he project is a collaboration between NASA, the European Space Agency (ESA), and the Canadian Space Agency.</p>
+          <p>the telescope is designed to observe stars and galaxies that formed over 13.5 billion years ago.</p>
+          <p>one of the main goals for the project is to study scientific mysteries revolving around the origins of the universe and where humans belong in it.</p>
+          <p>Webb can observe exoplanetary atmospheres (planets that orbit other stars) and learn about their chemical compositions through a technique known as transmission spectroscopy</p>
+          <p>the 18 mirrors of the Webb telescope are beryllium and gold-plated hexagonal shapes</p>
+          <p>it is the largest and most powerful space science telescope created by NASA</p>
+          <p>the building of the James Webb Space Telescope involved thousands of scientists and engineers from 29 U.S. states, 14 countries, and over 300 universities.</p>
+          <p>the Webb telescope is 100 times more powerful than the Hubble.</p>
+          <p>to protect the telescope from infrared radiation of the Sun, Earth, and Moon, the telescope has a 5-layer sunshield that equivalent to using SPF 1 million</p>
+          <p>Webb is the largest telescope in space</p>
+          <p>the Webb telescope operates at a temperature of -370°F (under 50 Kelvin)</p>
+          <p>the main instruments used to operate the Webb telescope are the near infrared camera (NIRCam), near infrared spectrograph (NIRSpec), the mid infrared instrument (MIRI), and the fine guidance sensors/near infrared imager and slitless spectrograph (FGS/NIRISS)</p>
+          <p>the building of the James Webb space telescope led to many major innovations in space technology including deployable sunshields, folding segmented mirrors, detectors, micro-shutters, mirror control, and lightweight optics</p>
+          <p>the telescope can be abbreviated as "JWST" or "Webb"</p>
+
+          <h2>here are some fun facts about the Hubble Telescope</h2>
+          <p></p>
         </div>
+        <!--
         <div class="row">
             <div class="column">
                  <div class="containerJWST">
                      <img src="jwst-08.jpeg" alt="jwst-08">
-                    <!--<button class="btn">click here</button>-->
+                    <button class="btn">click here</button>
                     <button class="jwstButton"><a href="jwstDescription.html">click this link</a></button>
                   </div>
             </div>
             <div class="column">
                 <div class="containerHubble">
                     <img src="jwst-08.jpeg" alt="jwst-08">
-                    <!--<button class="btn">click here</button>-->
+                    <button class="btn">click here</button>
                     <button class="hubbleButton"><a href="hubbleDescription.html">click this link</a></button>
                   </div>
             </div>
             <div class="column">
                 <div class="containerChandra">
                     <img src="jwst-08.jpeg" alt="jwst-08">
-                    <!--<button class="btn">click here</button>-->
+                    <button class="btn">click here</button>
                     <button class="chandraButton"><a href="chandraDescription.html">click this link</a></button>
                   </div>
             </div>
             <div class="column">
               <div class="containerSpitzer">
                   <img src="spitzer-09 2.jpeg" alt="jwst-08">
-                 <!--<button class="btn">click here</button>-->
+                 <<button class="btn">click here</button>
                  <button class="spitzerButton"><a href="spitzerDescription.html">click this link</a></button>
                </div>
          </div>
          <div class="column">
-          <div class="containerSpitzer">
+          <div class="containerJuno">
               <img src="spitzer-09 2.jpeg" alt="jwst-08">
-             <!--<button class="btn">click here</button>-->
-             <button class="spitzerButton"><a href="junoDescription.html">click this link</a></button>
+             <button class="btn">click here</button>-
+             <button class="junoButton"><a href="junoDescription.html">click this link</a></button>
            </div>
      </div>
 
      <div class="column">
-      <div class="containerSpitzer">
+      <div class="containerVoyager">
           <img src="spitzer-09 2.jpeg" alt="jwst-08">
-         <!--<button class="btn">click here</button>-->
-         <button class="spitzerButton"><a href="voyagerDescription.html">click this link</a></button>
-       </div>
+         <button class="btn">click here</button>
+         <button class="voyagerButton"><a href="voyagerDescription.html">click this link</a></button>
+       </div>-->
  </div>
      
-        </div>
+        </div> 
       </div>
 
 
-      <div class="content-area">
+      <!-- <div class="content-area">
         <div class="wrapper">
-          <!-- True or False Game -->
+           True or False Game 
           <h2>True or False?</h2>
           <p> NASA has built incredible telescopes and observatories with the help of many partners. Can you guess whether these facts are true or false? </p>
           <button id="tfButton">true or false</button>
           <p id="fact"></p>
           <p>facts are from: <a href="https://www.healthywa.wa.gov.au/Articles/A_E/Common-myths-about-sexual-assault">Common myths about sexual assault, Department of Health, Government of Western Australia</a></p>
       </div>
-    </div>
+    </div> -->
     <script src="script.js"></script>
   </body>
 </html>
