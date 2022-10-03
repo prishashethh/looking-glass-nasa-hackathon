b4ffc72420bd3baa9413dd34b17f5417a022318d
elishatherene <86765087+elishatherene@users.noreply.github.com>
2022-10-02 22:35:07 -0700

    Create factListVoyager.js

---
+++ factListVoyager.js
@@ -0,0 +1,15 @@
+var factList = [
+  "The Voyager mission involves twin spacecrafts known as Voyager 1 and Voyager 2, which were designed to explore parts of the outer solar system enivronment; past neighboring planets to the outer limits of the Sun's sphere and beyond.",
+  "The main mission was to explore Jupiter and Saturn. The spacecrafts discovered active volcanoes on Io (one of Jupiter's largest moons) and new details about Saturn's rings",
+  "Voyager 1 entered interstellar space (regions between stars where material from deaths of nearby stars is found) in 2012, and Voyager 2 crossed into interstellar space in 2018.",
+  "The spacecrafts' current project is the Voyager Interstellar Mission (VIM), which is desinged to observe the outer solar system environment and heliopause boundaries to gather more information about solar winds and interstellar field particles",
+  "The Voyager 2 was launched on August 20, 1977 and Voyager 1 was launcehd on September 5, 1977.",
+  "Voyager 1 and 2 have explored all the giant outer planets of our solar system, 48 of their moons, and their rings/magnetic field systems.",
+  "The original Voyager mission was planned to be four years, but the spacecrafts have currently been in space for 45 years. The extension of the mission was made possible through remote-control reprograming"
+];
+
+/* 
+sources:
+https://www.nasa.gov/mission_pages/voyager/overview
+
+*/
