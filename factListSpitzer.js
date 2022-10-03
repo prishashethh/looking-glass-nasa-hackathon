1e6e43d3277109f2708f14fde0dab9636cf080f9
elishatherene <86765087+elishatherene@users.noreply.github.com>
2022-10-02 21:00:22 -0700

    Create factListSpitzer.js

---
+++ factListSpitzer.js
@@ -0,0 +1,17 @@
+var factList = [
+  "The Spitzer Space Telescope is the last mission in the Great Observatories Program launched by NASA";
+  "The telescope was designed to detect infrared radiation (mostly heat radiation)";
+  "It is mostly made up of two major parts: The spacecraft (designed to control the telescope and communicate scientific data gathered) and the Cryogenic Telescope Assembly (an 85cm telescope built in with three other scientific instruments)";
+  "The Spitzer telescope discovered the first exoplanet weather map using heat radiation".
+  "The telescope enabled scientists to view dusty stellar nurseries, extrasolar planets, the centers of galaxies, newly formed planetary systems, failed stars, and organic molecules from other potential other planets with life; regions that cannot be seen through optical telescopes";
+  "The telescope's 'cold mission' lasted for 5.5 years, before shifting to the 'warm mission' for 11 years until it became renamed as the 'Spitzer Final Voyage'";
+  "Spitzer was launched on August 25, 2003 using a Delta II rocket";
+  "The telescope discovered a giant ring of Saturn and a system of Earth-sized planets approximately 40 light years away.";
+];
+
+/* 
+sources:
+https://www.nasa.gov/mission_pages/spitzer/infrared/index.html
+https://www.jpl.nasa.gov/news/15-of-spitzers-greatest-discoveries-from-15-years-in-space
+https://www.jpl.nasa.gov/missions/spitzer-space-telescope
+*/
