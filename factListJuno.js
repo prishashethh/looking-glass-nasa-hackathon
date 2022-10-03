ef128f3a290b1001666a6a96d97d1198067c8de5
elishatherene <86765087+elishatherene@users.noreply.github.com>
2022-10-02 21:46:26 -0700

    Update factListJuno.js

--- factListJuno.js
+++ factListJuno.js
@@ -6,7 +6,7 @@ var factList = [
   "The Juno mission was extended until September 2025 (or the end of its life) to enable the spacecraft to continue research the Jovian sustem and conduct flybys of Europa and Io (the two largest moons of Jupiter)",
   "Juno is a spinning, solar-powered spacecraft that uses a highly elliptical polar orbit (two rotations per minute) that is stable and easy to control. It is the first the first solar-powered spacecraft desgined by NASA to operate so far from the Sun",
   "The spacecraft is 66 ft (20 meters) in diameter and 15 ft (4.5 meters) in height",
-  "Juno uses many scienfitic instruments including a gravity science magnetometer (used to study the structure of Jupiter through the graity field), a JunoCam (to take close up images in color), a microwave radiometer (to observe the atmosphere/oxygen), a UVS/JIRAM ultraviolet/infrared camera (to take images of the aurora and atmosphere), and JEDI/JADE/Waves to observe the electrical fields and particles and connect the auroras to the magnetic field).",
+  "Juno uses many scienfitic instruments including a gravity science magnetometer (used to study the structure of Jupiter through the graity field), a JunoCam (to take close up images in color), a microwave radiometer (to observe the atmosphere/oxygen), a UVS/JIRAM ultraviolet/infrared camera (to take images of the aurora and atmosphere), and JEDI/JADE/Waves to observe the electrical fields and particles and connect the auroras to the magnetic field)."
 ];
 
  /* 
